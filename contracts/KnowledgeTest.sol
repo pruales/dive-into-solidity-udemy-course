5eb67444091a6a7f81d2e508fd983d91e7a8e4aa
Paul <paulruales1@gmail.com>
2022-08-05 14:08:10 -0700

    done

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -5,9 +5,38 @@ pragma solidity >=0.5.0 <0.9.0;
 contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
-
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    address public owner;
+ 
+    constructor() {
+        owner = msg.sender;
+    }
+ 
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+ 
+    function transferAll(address account) external {
+        require(msg.sender == owner, "ONLY_OWNER");
+        (bool result, ) = account.call{value: getBalance()}("");
+        require(result, "TX_FAILED");
+    }
+ 
+    function start() external {
+        players.push(msg.sender);
+    }
+ 
+    function concatenate(string calldata string1, string calldata string2)
+        external
+        pure
+        returns (string memory)
+    {
+        return string(abi.encodePacked(string1, string2));
+    }
+ 
+    function changeTokens() external {
+        string[] storage t = tokens;
         t[0] = "VET";
     }
+ 
+    receive() external payable {}
 }
